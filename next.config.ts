3901c35b2ed96ef1d83848460d09a216b18efba6
Occams-Razar <km31188@students.mcpasd.k12.wi.us>
2026-05-29 10:42:01 -0500

    package lock edit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-module.exports = {
-  allowedDevOrigins: ['100.98.54.58'],
-}
+// module.exports = {
+//   allowedDevOrigins: ['100.98.54.58'],
+// }
 const nextConfig: NextConfig = {
   devIndicators: false,
   
